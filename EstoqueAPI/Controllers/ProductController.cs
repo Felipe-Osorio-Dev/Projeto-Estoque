54d9847a05b35218c9a5c9001ce3ff620e65246a
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-06-21 15:49:45 -0300

    feat: Criado controller para expor os endpoints da aplicação.

---
+++ EstoqueAPI/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using EstoqueAPI.DTO;
+using EstoqueAPI.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EstoqueAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterProduct([FromBody] ProductDTO product)
+        {
+            await _service.RegisterProduct(product);
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
