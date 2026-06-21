893b31dc95ebfa8c5b970fc101086abe10195138
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-06-21 15:43:04 -0300

    feat: Criado Model para representar nossa tabela Produtos no banco de dados.

---
+++ EstoqueAPI/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EstoqueAPI.Models
+{
+    [Index(nameof(EAN), IsUnique = true)]
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string EAN { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+        public DateOnly CreateAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public bool IsEnable { get; set; } = false;
+    }
+}
