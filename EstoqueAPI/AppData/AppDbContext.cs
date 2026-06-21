039fe9847f6cc737b7806788412c2c7b4b565d35
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-06-21 15:43:44 -0300

    feat: Criado classe para o contexto do banco para a aplicação.

---
+++ EstoqueAPI/AppData/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EstoqueAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstoqueAPI.AppData
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options){}
+
+        public DbSet<ProductModel> Produtos { get; set; }
+    }
+}
