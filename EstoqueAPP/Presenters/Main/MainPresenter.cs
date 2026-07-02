fd2839b3629257e1a77a22edb23d831d75b38e02
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-02 16:58:44 -0300

    feat: Criado classe presenter para lidar com a logica da tela.

---
+++ EstoqueAPP/Presenters/Main/MainPresenter.cs
@@ -0,0 +1,24 @@
+﻿using EstoqueAPP.Services.Navigation;
+using EstoqueAPP.Views.Main;
+
+namespace EstoqueAPP.Presenters.Main
+{
+    internal class MainPresenter
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IMainView _mainView;
+
+        public MainPresenter(INavigationService navigationService, IMainView mainView)
+        {
+            _navigationService = navigationService;
+            _mainView = mainView;
+
+            _mainView.OpenDialogRegister += OpenDialogRegister;
+        }
+
+        private void OpenDialogRegister(object sender, EventArgs args)
+        {
+            _navigationService.OpenRegisterDialog();
+        }
+    }
+}
