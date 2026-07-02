f10cf25874c5ab9867bb1f3baccc8a5487d6d88b
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-02 16:59:31 -0300

    build: Configuração da Injeção de Dependencias do app.

--- EstoqueAPP/Program.cs
+++ EstoqueAPP/Program.cs
@@ -1,17 +1,48 @@
+using EstoqueAPP.Presenters.Main;
+using EstoqueAPP.Services.Navigation;
+using EstoqueAPP.Views.Main;
+using EstoqueAPP.Views.Register;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace EstoqueAPP
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            using (var scope = services.BuildServiceProvider())
+            {
+                ApplicationConfiguration.Initialize();
+
+                var mainContainer = scope.GetRequiredService<MainForm>();
+                scope.GetRequiredService<MainPresenter>();
+
+                Application.Run(mainContainer);
+            }
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            //Main Container
+            services.AddSingleton<MainForm>();
+
+            //Forms
+            services.AddTransient<RegisterForm>();
+
+            //Presenters
+            services.AddTransient<MainPresenter>();
+            
+            //Interfaces
+            services.AddTransient<IMainView>(sp => sp.GetRequiredService<MainForm>());
+
+            //Services
+            services.AddSingleton<INavigationService, NavigationService>();
         }
     }
 }
