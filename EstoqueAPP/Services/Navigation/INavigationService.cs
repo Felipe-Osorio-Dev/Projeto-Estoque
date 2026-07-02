b58c956678879fdc3ebd6b369a021dad428dedb4
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-02 16:56:34 -0300

    feat: Criado classe de serviço reponsavel pela navegação do app e implementação da navegação ate a tela de cadastro.

---
+++ EstoqueAPP/Services/Navigation/INavigationService.cs
@@ -0,0 +1,8 @@
+﻿namespace EstoqueAPP.Services.Navigation
+{
+    internal interface INavigationService
+    {
+        void NavigateTo(string key);
+        void OpenRegisterDialog();
+    }
+}
---
+++ EstoqueAPP/Services/Navigation/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using EstoqueAPP.Views.Register;
+
+namespace EstoqueAPP.Services.Navigation
+{
+    internal class NavigationService : INavigationService
+    {
+        public void NavigateTo(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenRegisterDialog()
+        {
+            using (RegisterForm registerForm = new RegisterForm())
+            {
+                if(registerForm.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+
+                MessageBox.Show("Testando");
+            }
+        }
+    }
+}
